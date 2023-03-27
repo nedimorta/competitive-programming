c63be3d55b939e08c30314d96b44e7fc4e6882e3
nedimorta <nedimorta@gmail.com>
2023-03-27 16:40:29 +0300

    Pairs problem is solved.

---
+++ HackerRank/domains/algorithms/search/medium/Pairs/Solution.java
@@ -0,0 +1,48 @@
+package HackerRank.domains.algorithms.search.medium.Pairs;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    static int pairs(int[] a,int k) {
+      int result = 0;
+      Arrays.sort(a);
+      
+      for(int number : a){
+          if(Arrays.binarySearch(a, number - k) > -1){
+              result++;
+          }
+      }
+      
+      
+      return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int res;
+        
+        String n = in.nextLine();
+        String[] n_split = n.split(" ");
+        
+        int _a_size = Integer.parseInt(n_split[0]);
+        int _k = Integer.parseInt(n_split[1]);
+        
+        int[] _a = new int[_a_size];
+        int _a_item;
+        String next = in.nextLine();
+        String[] next_split = next.split(" ");
+        
+        for(int _a_i = 0; _a_i < _a_size; _a_i++) {
+            _a_item = Integer.parseInt(next_split[_a_i]);
+            _a[_a_i] = _a_item;
+        }
+        
+        res = pairs(_a,_k);
+        System.out.println(res);
+    }
+}
+
