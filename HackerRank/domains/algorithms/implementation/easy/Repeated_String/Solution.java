1ff318cb20197f4ce7a0e9739e8ac33364877297
nedimorta <nedimorta@gmail.com>
2023-04-05 00:40:18 +0300

    Repeated Strings problem revised.

--- HackerRank/domains/algorithms/implementation/easy/Repeated_String/Solution.java
+++ HackerRank/domains/algorithms/implementation/easy/Repeated_String/Solution.java
@@ -4,53 +4,64 @@ import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.function.*;
 import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class Solution {
+class Result {
 
-    // Complete the repeatedString function below.
-    static long repeatedString(String s, long n) {
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+    public static long repeatedString(String s, long n) {
         long division = (long)Math.floor(n / s.length());
         long remainder = n % s.length();
         long numberOfAs = 0;
-        
+
         for(char ch : s.toCharArray()) {
             if(ch == 'a') {
                 numberOfAs++;
             }
         }
-        
+
         long result = division * numberOfAs;
-        
+
         for(int i = 0; i < remainder; i++) {
             char ch = s.charAt(i);
-            
+
             if(ch == 'a') {
                 result++;
             }
         }
-        
-        return result;
 
+        return result;
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
+}
 
+public class Solution {
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String s = scanner.nextLine();
+        String s = bufferedReader.readLine();
 
-        long n = scanner.nextLong();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        long n = Long.parseLong(bufferedReader.readLine().trim());
 
-        long result = repeatedString(s, n);
+        long result = Result.repeatedString(s, n);
 
         bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
 
+        bufferedReader.close();
         bufferedWriter.close();
-
-        scanner.close();
     }
 }
